b2c0ff11220003d265289990a5ced2ee9ee97f05
zguaidh <zguaidhabiba@gmail.com>
2024-08-06 15:43:51 +0000

    python async comprehension tasks

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,12 +3,13 @@
 10 random numbers from async_generator then returns these numbers'''
 
 
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     '''asynchronous return the numbers from async_generator'''
-    num = []
+    result = []
     async for num in async_generator():
-        num.append(num)
-    return num
+        result.append(num)
+    return result
--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -8,7 +8,7 @@ import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime():
+async def measure_runtime() -> float:
     '''return the total run time of 4 async_comprehension in parallel'''
     start_time = time.perf_counter()
     await asyncio.gather(async_comprehension(), async_comprehension(),
