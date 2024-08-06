c27ec138fdd7ec93e384565057bf5268ca998256
zguaidh <zguaidhabiba@gmail.com>
2024-08-06 15:05:32 +0000

    python async comprehension tasks

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,8 +7,8 @@ import asyncio
 import random
 
 
-async def async_generator():
+async def async_generator() -> float:
     '''yields a random number with a 1 second delay'''
     for i in range(10):
         await asyncio.sleep(1)
-        yield random(0, 10)
+        yield random.uniform(0, 10)
